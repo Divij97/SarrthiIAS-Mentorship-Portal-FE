9b0fa37dcbff4d0007bc12cc960d368faf1106e7
vijay-kartik <kartik.iit96@gmail.com>
2025-04-11 15:09:45 +0530

    create schedule biweekly bug

--- src/components/Home/AddScheduleForMentee.tsx
+++ src/components/Home/AddScheduleForMentee.tsx
@@ -208,7 +208,7 @@ export default function AddScheduleForMentee({
                         className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6"
                       >
                         <option value="WEEKLY">Weekly</option>
-                        <option value="BIWEEKLY">Bi-weekly</option>
+                        <option value="BI_WEEKLY">Bi-weekly</option>
                       </select>
                     </div>
 
